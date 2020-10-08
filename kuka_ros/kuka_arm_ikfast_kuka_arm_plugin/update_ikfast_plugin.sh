79f3b6e7e8ea0e7f0a2c5afb4e03039410318913
rohanyuttham <rohan989yuttham@gmail.com>
2020-10-09 02:27:52 +0530

    added ikfastsolver

---
+++ kuka_ros/kuka_arm_ikfast_kuka_arm_plugin/update_ikfast_plugin.sh
@@ -0,0 +1,1 @@
+rosrun moveit_kinematics create_ikfast_moveit_plugin.py kuka_arm kuka_arm kuka_arm_ikfast_kuka_arm_plugin /home/rohan/catkin_ws/src/multiprocess-robotic-cell/kuka_ros/kuka_arm_ikfast_kuka_arm_plugin/src/kuka_arm_kuka_arm_ikfast_solver.cpp
